1be22a67d979883100c0464db9de49cfef4db131
Fatih Turker <fatihturker35@gmail.com>
2020-12-28 16:22:49 +0300

    Fix security issue

--- app/util/mail.util.ts
+++ app/util/mail.util.ts
@@ -22,7 +22,6 @@ export class MailUtil {
     this.config = {
       host: process.env.MAIL_HOST,
       port: process.env.MAIL_PORT,
-      secure: false, // true for 465, false for other ports
       auth: {
         user: process.env.MAIL_USERNAME,
         pass: process.env.MAIL_PASSWORD,
