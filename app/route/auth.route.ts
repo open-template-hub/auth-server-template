5ea9a6bc75329720c40f937c67690dc2e7e71b57
Fatih Turker <fatihturker35@gmail.com>
2021-09-22 13:35:43 +0300

    Implement MQ consumer and mail publish

--- app/route/auth.route.ts
+++ app/route/auth.route.ts
@@ -17,7 +17,7 @@ const subRoutes = {
   forgetPassword: '/forget-password',
   resetPassword: '/reset-password',
   resetPasswordToken: '/reset-password-token',
-  user: '/user'
+  user: '/user',
 };
 
 export const publicRoutes = [
@@ -30,123 +30,126 @@ export const publicRoutes = [
   subRoutes.resetPassword,
 ];
 
-export const adminRoutes = [
-  subRoutes.resetPasswordToken,
-  subRoutes.user
-];
+export const adminRoutes = [subRoutes.resetPasswordToken, subRoutes.user];
 
 export const router = Router();
 
-router.post( subRoutes.signup, async ( req: Request, res: Response ) => {
+router.post(subRoutes.signup, async (req: Request, res: Response) => {
   // sign up
   const authController = new AuthController();
   const context = res.locals.ctx;
-  const response = await authController.signup( context.postgresql_provider, {
-    username: req.body.username,
-    password: req.body.password,
-    email: req.body.email,
-  } as User );
-  res.status( ResponseCode.CREATED ).json( response );
-} );
+  const response = await authController.signup(
+    context.postgresql_provider,
+    context.message_queue_provider,
+    {
+      username: req.body.username,
+      password: req.body.password,
+      email: req.body.email,
+    } as User
+  );
+  res.status(ResponseCode.CREATED).json(response);
+});
 
-router.post( subRoutes.login, async ( req: Request, res: Response ) => {
+router.post(subRoutes.login, async (req: Request, res: Response) => {
   // login
   const authController = new AuthController();
   const context = res.locals.ctx;
-  const response = await authController.login( context.postgresql_provider, {
+  const response = await authController.login(context.postgresql_provider, {
     username: req.body.username,
     password: req.body.password,
     email: req.body.email,
-  } as User );
-  res.status( ResponseCode.OK ).json( {
+  } as User);
+  res.status(ResponseCode.OK).json({
     accessToken: response.accessToken,
     refreshToken: response.refreshToken,
-  } );
-} );
+  });
+});
 
-router.post( subRoutes.logout, async ( req: Request, res: Response ) => {
+router.post(subRoutes.logout, async (req: Request, res: Response) => {
   //  logout
   const authController = new AuthController();
   const context = res.locals.ctx;
-  await authController.logout( context.postgresql_provider, req.body.token );
-  res.status( ResponseCode.NO_CONTENT ).json( {} );
-} );
+  await authController.logout(context.postgresql_provider, req.body.token);
+  res.status(ResponseCode.NO_CONTENT).json({});
+});
 
-router.post( subRoutes.token, async ( req: Request, res: Response ) => {
+router.post(subRoutes.token, async (req: Request, res: Response) => {
   // get token
   const authController = new AuthController();
   const context = res.locals.ctx;
   const accessToken = await authController.token(
-      context.postgresql_provider,
-      req.body.token
+    context.postgresql_provider,
+    req.body.token
   );
   res
-  .status( ResponseCode.OK )
-  .json( { accessToken: accessToken, refreshToken: req.body.token } );
-} );
+    .status(ResponseCode.OK)
+    .json({ accessToken: accessToken, refreshToken: req.body.token });
+});
 
-router.get( subRoutes.verify, async ( req: Request, res: Response ) => {
+router.get(subRoutes.verify, async (req: Request, res: Response) => {
   // verify token
   const authController = new AuthController();
   const context = res.locals.ctx;
   await authController.verify(
-      context.postgresql_provider,
-      req.query.token as string
+    context.postgresql_provider,
+    req.query.token as string
   );
-  res.status( ResponseCode.NO_CONTENT ).json( {} );
-} );
+  res.status(ResponseCode.NO_CONTENT).json({});
+});
 
-router.post( subRoutes.forgetPassword, async ( req: Request, res: Response ) => {
+router.post(subRoutes.forgetPassword, async (req: Request, res: Response) => {
   // forget password
   const authController = new AuthController();
   const context = res.locals.ctx;
   await authController.forgetPassword(
-      context.postgresql_provider,
-      req.body.username
+    context.postgresql_provider,
+    context.message_queue_provider,
+    req.body.username
   );
-  res.status( ResponseCode.NO_CONTENT ).json( {} );
-} );
+  res.status(ResponseCode.NO_CONTENT).json({});
+});
 
 router.get(
-    subRoutes.resetPasswordToken,
-    async ( req: Request, res: Response ) => {
-      // generate reset password token
-      const authController = new AuthController();
-      const context = res.locals.ctx;
-      const resetPasswordToken = await authController.forgetPassword(
-          context.postgresql_provider,
-          req.query.username as string,
-          false
-      );
-      res.status( ResponseCode.OK ).json( { resetPasswordToken } );
-    }
+  subRoutes.resetPasswordToken,
+  async (req: Request, res: Response) => {
+    // generate reset password token
+    const authController = new AuthController();
+    const context = res.locals.ctx;
+    const resetPasswordToken = await authController.forgetPassword(
+      context.postgresql_provider,
+      context.message_queue_provider,
+      req.query.username as string,
+      true
+    );
+    res.status(ResponseCode.OK).json({ resetPasswordToken });
+  }
 );
 
-router.post( subRoutes.resetPassword, async ( req: Request, res: Response ) => {
+router.post(subRoutes.resetPassword, async (req: Request, res: Response) => {
   // reset password
   const authController = new AuthController();
   const context = res.locals.ctx;
   await authController.resetPassword(
-      context.postgresql_provider,
-      {
-        username: req.body.username,
-        password: req.body.password,
-      } as User,
-      req.body.token
+    context.postgresql_provider,
+    {
+      username: req.body.username,
+      password: req.body.password,
+    } as User,
+    req.body.token
   );
-  res.status( ResponseCode.NO_CONTENT ).json( {} );
-} );
+  res.status(ResponseCode.NO_CONTENT).json({});
+});
 
-router.delete( subRoutes.user, async ( req: Request, res: Response ) => {
+router.delete(subRoutes.user, async (req: Request, res: Response) => {
   // delete user
   const authController = new AuthController();
   const context = res.locals.ctx;
   await authController.deleteUser(
-      context.postgresql_provider,
-      context.username,
-      {
-        username: req.query.username,
-      } as User
+    context.postgresql_provider,
+    context.username,
+    {
+      username: req.query.username,
+    } as User
   );
-  res.status( ResponseCode.NO_CONTENT ).json( {} );
-} );
+  res.status(ResponseCode.NO_CONTENT).json({});
+});
--- app/route/index.route.ts
+++ app/route/index.route.ts
@@ -2,13 +2,31 @@
  * @description holds index routes
  */
 
-import { context, EncryptionUtil, ErrorHandlerUtil, PostgreSqlProvider, PreloadUtil, } from '@open-template-hub/common';
+import {
+  context,
+  EncryptionUtil,
+  ErrorHandlerUtil,
+  MessageQueueProvider,
+  PostgreSqlProvider,
+  PreloadUtil,
+} from '@open-template-hub/common';
 import { NextFunction, Request, Response } from 'express';
 import { Environment } from '../../environment';
-import { adminRoutes as authAdminRoutes, publicRoutes as authPublicRoutes, router as authRouter, } from './auth.route';
+import { AuthQueueConsumer } from '../consumer/auth-queue.consumer';
+import {
+  adminRoutes as authAdminRoutes,
+  publicRoutes as authPublicRoutes,
+  router as authRouter,
+} from './auth.route';
 import { router as infoRouter } from './info.route';
-import { publicRoutes as monitorPublicRoutes, router as monitorRouter, } from './monitor.route';
-import { publicRoutes as socialLoginPublicRoutes, router as socialLoginRouter, } from './social-login.route';
+import {
+  publicRoutes as monitorPublicRoutes,
+  router as monitorRouter,
+} from './monitor.route';
+import {
+  publicRoutes as socialLoginPublicRoutes,
+  router as socialLoginRouter,
+} from './social-login.route';
 
 const subRoutes = {
   root: '/',
@@ -20,102 +38,116 @@ const subRoutes = {
 
 export namespace Routes {
   var environment: Environment;
+  var message_queue_provider: MessageQueueProvider;
   var postgresql_provider: PostgreSqlProvider;
   const errorHandlerUtil = new ErrorHandlerUtil();
 
   var publicRoutes: string[] = [];
   var adminRoutes: string[] = [];
 
-  function populateRoutes( mainRoute: string, routes: Array<string> ) {
+  function populateRoutes(mainRoute: string, routes: Array<string>) {
     var populated = Array<string>();
-    for ( const s of routes ) {
-      populated.push( mainRoute + ( s === '/' ? '' : s ) );
+    for (const s of routes) {
+      populated.push(mainRoute + (s === '/' ? '' : s));
     }
 
     return populated;
   }
 
-  export function mount( app: any ) {
-    const preloadUtil = new PreloadUtil();
+  export function mount(app: any) {
     environment = new Environment();
 
+    message_queue_provider = new MessageQueueProvider(environment.args());
+
+    const channelTag = new Environment().args().mqArgs
+      ?.authServerMessageQueueChannel as string;
+    message_queue_provider.getChannel(channelTag).then((channel: any) => {
+      const authQueueConsumer = new AuthQueueConsumer(channel);
+      message_queue_provider.consume(
+        channel,
+        channelTag,
+        authQueueConsumer.onMessage,
+        1
+      );
+    });
+    const preloadUtil = new PreloadUtil();
+
     postgresql_provider = new PostgreSqlProvider(
-        environment.args(),
-        'AuthServer'
+      environment.args(),
+      'AuthServer'
     );
 
     preloadUtil
-    .preload( undefined, postgresql_provider )
-    .then( () => console.log( 'DB preloads are completed.' ) );
+      .preload(undefined, postgresql_provider)
+      .then(() => console.log('DB preloads are completed.'));
 
     publicRoutes = [
-      ...populateRoutes( subRoutes.monitor, monitorPublicRoutes ),
-      ...populateRoutes( subRoutes.auth, authPublicRoutes ),
-      ...populateRoutes( subRoutes.social, socialLoginPublicRoutes ),
+      ...populateRoutes(subRoutes.monitor, monitorPublicRoutes),
+      ...populateRoutes(subRoutes.auth, authPublicRoutes),
+      ...populateRoutes(subRoutes.social, socialLoginPublicRoutes),
     ];
-    console.log( 'Public Routes: ', publicRoutes );
+    console.log('Public Routes: ', publicRoutes);
 
-    adminRoutes = [
-      ...populateRoutes( subRoutes.auth, authAdminRoutes )
-    ];
-    console.log( 'Admin Routes: ', adminRoutes );
+    adminRoutes = [...populateRoutes(subRoutes.auth, authAdminRoutes)];
+    console.log('Admin Routes: ', adminRoutes);
 
     const responseInterceptor = (
-        req: Request,
-        res: Response,
-        next: NextFunction
+      req: Request,
+      res: Response,
+      next: NextFunction
     ) => {
       var originalSend = res.send;
-      const encryptionUtil = new EncryptionUtil( environment.args() );
+      const encryptionUtil = new EncryptionUtil(environment.args());
       res.send = function () {
-        console.log( 'Starting Encryption: ', new Date() );
-        let encrypted_arguments = encryptionUtil.encrypt( arguments );
-        console.log( 'Encryption Completed: ', new Date() );
+        console.log('Starting Encryption: ', new Date());
+        let encrypted_arguments = encryptionUtil.encrypt(arguments);
+        console.log('Encryption Completed: ', new Date());
 
-        originalSend.apply( res, encrypted_arguments as any );
+        originalSend.apply(res, encrypted_arguments as any);
       } as any;
 
       next();
     };
 
     // Use this interceptor before routes
-    app.use( responseInterceptor );
+    app.use(responseInterceptor);
 
     // INFO: Keep this method at top at all times
-    app.all( '/*', async ( req: Request, res: Response, next: NextFunction ) => {
+    app.all('/*', async (req: Request, res: Response, next: NextFunction) => {
       try {
         // create context
         res.locals.ctx = await context(
-            req,
-            environment.args(),
-            publicRoutes,
-            adminRoutes,
-            undefined,
-            postgresql_provider
+          req,
+          environment.args(),
+          publicRoutes,
+          adminRoutes,
+          undefined,
+          postgresql_provider,
+          message_queue_provider
         );
 
         next();
-      } catch ( err ) {
-        let error = errorHandlerUtil.handle( err );
-        res.status( error.code ).json( { message: error.message } );
+      } catch (err) {
+        let error = errorHandlerUtil.handle(err);
+        res.status(error.code).json({ message: error.message });
       }
-    } );
+    });
 
     // INFO: Add your routes here
-    app.use( subRoutes.monitor, monitorRouter );
-    app.use( subRoutes.auth, authRouter );
-    app.use( subRoutes.social, socialLoginRouter );
-    app.use( subRoutes.info, infoRouter );
+    app.use(subRoutes.monitor, monitorRouter);
+    app.use(subRoutes.auth, authRouter);
+    app.use(subRoutes.social, socialLoginRouter);
+    app.use(subRoutes.info, infoRouter);
 
     // Use for error handling
-    app.use( function (
-        err: Error,
-        req: Request,
-        res: Response,
-        next: NextFunction
+    app.use(function (
+      err: Error,
+      req: Request,
+      res: Response,
+      next: NextFunction
     ) {
-      let error = errorHandlerUtil.handle( err );
-      res.status( error.code ).json( { message: error.message } );
-    } );
+      let error = errorHandlerUtil.handle(err);
+      res.status(error.code).json({ message: error.message });
+    });
   }
 }
