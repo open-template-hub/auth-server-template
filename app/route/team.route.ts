bca6d64e0012be28bc4bf6efef89aadf177b3673
Mert Saraç <mertsarac@yandex.com>
2023-01-08 16:46:31 +0300

    added authorizedBy to team routers

--- app/route/team.route.ts
+++ app/route/team.route.ts
@@ -1,4 +1,4 @@
-import { ResponseCode, teamAuthorizedBy, TeamRole } from '@open-template-hub/common';
+import { authorizedBy, ResponseCode, teamAuthorizedBy, TeamRole, UserRole } from '@open-template-hub/common';
 import { Request, Response } from 'express';
 import Router from 'express-promise-router';
 import { TeamController } from '../controller/team.controller';
@@ -12,7 +12,7 @@ const subRoutes = {
 
 export const router = Router();
 
-router.get( subRoutes.root, async ( req: Request, res: Response ) => {
+router.get( subRoutes.root, authorizedBy( [ UserRole.ADMIN, UserRole.DEFAULT ] ), async ( req: Request, res: Response ) => {
   const context = res.locals.ctx;
 
   const teams = await TeamController.getTeams(
@@ -23,7 +23,7 @@ router.get( subRoutes.root, async ( req: Request, res: Response ) => {
   res.status( ResponseCode.OK ).json( teams );
 } );
 
-router.post( subRoutes.root, async ( req: Request, res: Response ) => {
+router.post( subRoutes.root, authorizedBy( [ UserRole.ADMIN, UserRole.DEFAULT ] ), async ( req: Request, res: Response ) => {
   const teamController = new TeamController();
 
   const teamCreateResponse = await teamController.create(
@@ -35,7 +35,7 @@ router.post( subRoutes.root, async ( req: Request, res: Response ) => {
   res.status( ResponseCode.OK ).json( teamCreateResponse );
 } );
 
-router.delete( subRoutes.root, teamAuthorizedBy( [ TeamRole.CREATOR, TeamRole.READER, TeamRole.WRITER ] ), async ( req: Request, res: Response ) => {
+router.delete( subRoutes.root, authorizedBy( [ UserRole.ADMIN, UserRole.DEFAULT ] ), teamAuthorizedBy( [ TeamRole.CREATOR ] ), async ( req: Request, res: Response ) => {
   const teamController = new TeamController();
 
   await teamController.deleteTeam(
@@ -45,7 +45,7 @@ router.delete( subRoutes.root, teamAuthorizedBy( [ TeamRole.CREATOR, TeamRole.RE
   res.status( ResponseCode.OK );
 } );
 
-router.post( subRoutes.writer, teamAuthorizedBy( [ TeamRole.CREATOR ] ), async ( req: Request, res: Response ) => {
+router.post( subRoutes.writer, authorizedBy( [ UserRole.ADMIN, UserRole.DEFAULT ] ), teamAuthorizedBy( [ TeamRole.CREATOR ] ), async ( req: Request, res: Response ) => {
   const teamController = new TeamController();
 
   const addWriterResponse = await teamController.addWriter(
@@ -59,7 +59,7 @@ router.post( subRoutes.writer, teamAuthorizedBy( [ TeamRole.CREATOR ] ), async (
   res.status( ResponseCode.OK ).json( addWriterResponse );
 } );
 
-router.post( subRoutes.reader, teamAuthorizedBy( [ TeamRole.CREATOR ] ), async ( req: Request, res: Response ) => {
+router.post( subRoutes.reader, authorizedBy( [ UserRole.ADMIN, UserRole.DEFAULT ] ), teamAuthorizedBy( [ TeamRole.CREATOR ] ), async ( req: Request, res: Response ) => {
   const teamController = new TeamController();
 
   const addReaderResponse = await teamController.addReader(
@@ -73,7 +73,7 @@ router.post( subRoutes.reader, teamAuthorizedBy( [ TeamRole.CREATOR ] ), async (
   res.status( ResponseCode.OK ).json( addReaderResponse );
 } );
 
-router.delete( subRoutes.writer, teamAuthorizedBy( [ TeamRole.CREATOR ] ), async ( req: Request, res: Response ) => {
+router.delete( subRoutes.writer, authorizedBy( [ UserRole.ADMIN, UserRole.DEFAULT ] ), teamAuthorizedBy( [ TeamRole.CREATOR ] ), async ( req: Request, res: Response ) => {
   const teamController = new TeamController();
 
   await teamController.removeWriter(
@@ -85,7 +85,7 @@ router.delete( subRoutes.writer, teamAuthorizedBy( [ TeamRole.CREATOR ] ), async
   res.status( ResponseCode.OK );
 } );
 
-router.delete( subRoutes.reader, teamAuthorizedBy( [ TeamRole.CREATOR ] ), async ( req: Request, res: Response ) => {
+router.delete( subRoutes.reader, authorizedBy( [ UserRole.ADMIN, UserRole.DEFAULT ] ), teamAuthorizedBy( [ TeamRole.CREATOR ] ), async ( req: Request, res: Response ) => {
   const teamController = new TeamController();
 
   await teamController.removeReader(
@@ -97,7 +97,7 @@ router.delete( subRoutes.reader, teamAuthorizedBy( [ TeamRole.CREATOR ] ), async
   res.status( ResponseCode.OK );
 } );
 
-router.post( subRoutes.verify, async ( req: Request, res: Response ) => {
+router.post( subRoutes.verify, authorizedBy( [ UserRole.ADMIN, UserRole.DEFAULT ] ), async ( req: Request, res: Response ) => {
   const teamController = new TeamController();
   const context = res.locals.ctx;
 
